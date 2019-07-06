911f38c9fd7831068c367cd81043207b3915886f
Виталий Приходьков <prihodkov21vek@gmail.com>
2019-07-06 11:23:14 +0400

    Добавьте файлы проекта.

---
+++ Example36.cpp
@@ -0,0 +1,26 @@
+#include <conio.h>
+#include <stdio.h>
+
+
+
+long int fact(int n);
+
+int main()
+{
+	int num;
+	printf("Enter integer positive number: ");
+	scanf("%d", &num);
+	printf("Factorial = %ld\n", fact(num));
+	
+	getch();
+	return 0;
+}
+
+long int fact(int n)
+{
+	if(n > 0)
+		return n*fact(n-1);
+	else
+		return 1;
+}
+
